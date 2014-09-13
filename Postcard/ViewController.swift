21c1f1ea77f022a57a957c117ccf3f4a563d9383
Simon Lally <Simon@simons-mbp.home>
2014-09-13 17:26:14 -0400

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var messageButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMessageButtonPressed(sender: UIButton) {
+        // Code will evaluate when button is pressed
+        
+        // when send message is pressed label will appear
+        messageLabel.hidden = false
+        
+        // set the label's text to display the string entered in enterMessageTextField
+        messageLabel.text = enterMessageTextField.text
+        
+        enterMessageTextField.text = ""
+        // have the keyboard hide
+        enterMessageTextField.resignFirstResponder()
+        // have the return text in the label red
+        messageLabel.textColor = UIColor.redColor()
+        
+        // functions in swifts are easily identified with parenthesis 
+        messageButton.setTitle("Message Sent", forState: UIControlState.Normal)
+        
+    }
 
 }
 
